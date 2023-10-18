7eea37e8156208980c99bec1dd822c994890ac76
Mars Alexis Regidor <pythonista.mars@gmail.com>
2023-10-18 22:52:41 +0800

    vendor approval by admin, partial dashboards

--- vendor/models.py
+++ vendor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from accounts.models import User, UserProfile
+from accounts.utils import send_notification
 
 # Create your models here.
 class Vendor(models.Model):
@@ -14,3 +15,23 @@ class Vendor(models.Model):
     def __str__(self):
         return self.vendor_name
 
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            orig = Vendor.objects.get(pk=self.pk)
+            if orig.is_approved != self.is_approved:
+                # There's an Update
+                mail_template = 'admin_approval_email'
+                context = {
+                    'user': self.user,
+                    'is_approved': self.is_approved
+                }
+                if self.is_approved == True:
+                    # send notif
+                    mail_subject = "Congratulations! Your restaurant is now approved"
+                    send_notification(mail_subject, mail_template, context)
+                else:
+                    # send notif
+                    mail_subject = "Sorry! Your restaurant is not eligible for publishing your food menu or our marketplace"
+                    send_notification(mail_subject, mail_template, context)
+
+        return super(Vendor, self).save(*args, **kwargs)
