f8fe1583d91d5ed53659a49478351007a3f486a0
Mars Alexis Regidor <pythonista.mars@gmail.com>
2023-12-18 20:08:13 +0800

    customer dashboard

--- customers/urls.py
+++ customers/urls.py
@@ -7,4 +7,6 @@ from . import views
 urlpatterns = [
     path('', AccountViews.customerDashboard, name='customer'),
     path('profile/', views.cprofile, name='cprofile'),
+    path('my_orders/', views.my_orders, name='customer_my_orders'),
+    path('order_detail/<int:order_number>/', views.order_detail, name='order_detail'),
 ]
