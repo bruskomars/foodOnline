2e6d049071cd4a4f838c2665aca8e816b37fb28f
Mars Alexis Regidor <pythonista.mars@gmail.com>
2024-01-17 21:30:50 +0800

    email template implementation

--- orders/views.py
+++ orders/views.py
@@ -9,6 +9,7 @@ from .utils import generate_order_number
 from django.http import HttpResponse, JsonResponse
 from accounts.utils import send_notification
 from django.contrib.auth.decorators import login_required
+from django.contrib.sites.shortcuts import get_current_site
 
 # Create your views here.
 @login_required(login_url='login')
@@ -139,10 +140,22 @@ def payments(request):
         # SEND ORDER CONFIRMATION EMAIL TO THE CUSTOMER
         mail_subject = 'Thank you for ordering with us.'
         mail_template = 'orders/order_confirmation_email.html'
+
+        ordered_food = OrderedFood.objects.filter(order=order)
+        customer_subtotal = 0
+
+        for item in ordered_food:
+            customer_subtotal += (item.price * item.quantity)
+
+        tax_data = json.loads(order.tax_data)
         context = {
             'user': request.user,
             'order': order,
             'to_email': order.email,
+            'ordered_food':ordered_food,
+            'domain': get_current_site(request),
+            'customer_subtotal': customer_subtotal,
+            'tax_data':tax_data,
         }
         send_notification(mail_subject, mail_template, context)
 
